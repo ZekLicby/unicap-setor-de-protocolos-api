ea2953d255934f88b093a1f03a6c6d50cbe63691
Carlos Lins <carloslinsalbertoa@gmail.com>
2023-12-10 17:49:07 -0300

    Correção de configuração para deploy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infra/modules/app.module';
 import * as express from 'express';
@@ -9,7 +10,8 @@ dotenv.config();
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const PORT = process.env.PORT || 3001;
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
